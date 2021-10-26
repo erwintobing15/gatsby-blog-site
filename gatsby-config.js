9ad8f01124f47e5e649a521de48c9c11d49e43ce
erwintobing15 <erwintobing15@gmail.com>
2021-10-26 17:18:30 +0700

    Use useStaticQuery to pull the site title into the Layout component

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "Gatsby Blog Site",
+    title: "Destination Blog Site",
   },
   plugins: [
     "gatsby-plugin-image",
